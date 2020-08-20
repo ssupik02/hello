ce729297147b6ae4e4142b3270cb8cee615f9758
Spencer Supik <ssupik2@huskers.unl.edu>
2020-08-20 22:18:37 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Spencer Supik
+ *         ssupik2@huskers.unl.edu
+ * Date: 08/17/2020
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Spencer Supik!\n");
+
+  return 0;
+}
